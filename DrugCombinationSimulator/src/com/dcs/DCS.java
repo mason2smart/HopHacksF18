0d06ece65c5b7ee999e3f9043261a398bd8810c8
Mason Cole <mason.s.cole@hotmail.com>
2018-09-15 13:08:32 -0400

    Framework

--- DrugCombinationSimulator/src/com/dcs/DCS.java
+++ DrugCombinationSimulator/src/com/dcs/DCS.java
@@ -7,5 +7,6 @@ public class DCS {
 
     public static void main(String[] args) {
         JFrame selection = new Framework();
+        selection.
     }
 }
--- DrugCombinationSimulator/src/com/dcs/Framework.java
+++ DrugCombinationSimulator/src/com/dcs/Framework.java
@@ -4,9 +4,18 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Framework extends JFrame {
-
-
+    Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize(); //for scaling
+    final double FrameWidthMulti = screenSize.getWidth() / 1920;//scaling
+    final double FrameHeightMulti = screenSize.getHeight() / 1080;//scaling
+    final int FrameWidth = (int) (1000 * FrameWidthMulti);
+    final int FrameHeight = (int) (800 * FrameHeightMulti);
+    Dimension windowSize;
     public Framework(){ //constructor
+        super("DCS");
+        this.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE); //for now
+            //ADD CLOSE CONFIRMATION HERE - WINDOW LISTENER
+        windowSize = new Dimension(FrameWidth, FrameHeight);
+
     }
 
 }
