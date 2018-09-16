b15ccebf33d9d4dc390b0c57303ea2cdca577658
Mason Cole <mason.s.cole@hotmail.com>
2018-09-15 20:54:49 -0400

    Fixed Backend
    Fraction to Decimal String Conversion

--- DrugCombinationSimulator/src/com/dcs/Backend.java
+++ DrugCombinationSimulator/src/com/dcs/Backend.java
@@ -1,27 +1,45 @@
 package com.dcs;
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import com.sun.org.apache.bcel.internal.util.ClassPath;
+
+import java.io.*;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.Scanner;
+
+import static sun.plugin.javascript.navig.JSType.URL;
 
 public class Backend {
 
     public static ArrayList<Drug> getDrugs() {
         ArrayList<Drug> drugArr = new ArrayList();
         try {
-            BufferedReader input = new BufferedReader( new FileReader("drug_data.txt"));
-            String currentDrug;
-            while ((currentDrug = input.readLine()) != null) {
+            InputStream inputStream = ClassLoader.getSystemClassLoader().getResourceAsStream("drug_data.txt");
+            String currentDrug = "";
+            Scanner scannie = new Scanner(inputStream);
+
+            while(scannie.hasNextLine()) {
+                currentDrug = scannie.nextLine();
                 String[] textArr = currentDrug.split(" ");
-                Drug newDrug = new Drug(textArr[0], Double.parseDouble(textArr[1]), Double.parseDouble(textArr[2]), Double.parseDouble(textArr[3]));
+                Drug newDrug = new Drug(textArr[0], parseDouble(textArr[1]), parseDouble(textArr[2]), parseDouble(textArr[3]));
                 drugArr.add(newDrug);
             }
+            scannie.close();
             return drugArr;
+
         }
-        catch (IOException e) {
+        catch (Exception e) {
             e.printStackTrace();
             System.out.println("File I/O Error");
         }
         return null;
+    }
+    static double parseDouble(String ratio) {
+        if (ratio.contains("/")) {
+            String[] rat = ratio.split("/");
+            return Double.parseDouble(rat[0]) / Double.parseDouble(rat[1]);
+        } else {
+            return Double.parseDouble(ratio);
+        }
     }
 }
